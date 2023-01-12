eb537a5d2f1c9412ed976ba6eee310e261aa4f31
Ksiyeong <yuiop2767@naver.com>
2023-01-12 21:45:48 +0900

    Chore: 세팅

---
+++ server/linode-github-action-test/src/main/java/com/eroom/linodegithubactiontest/LinodeGithubActionTestApplication.java
@@ -0,0 +1,13 @@
+package com.eroom.linodegithubactiontest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LinodeGithubActionTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LinodeGithubActionTestApplication.class, args);
+	}
+
+}
---
+++ server/linode-github-action-test/src/test/java/com/eroom/linodegithubactiontest/LinodeGithubActionTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eroom.linodegithubactiontest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LinodeGithubActionTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
